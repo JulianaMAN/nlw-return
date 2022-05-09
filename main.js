c65779bf77eb6c56e3ef727f616871da4946a709
unknown <jmalencarn@gmail.com>
2022-05-09 16:24:12 -0300

    stage 5

--- main.js
+++ main.js
@@ -4,7 +4,53 @@ onScroll();
 function onScroll() {
   showNavOnScroll();
   showBackToTopButtonOnScroll();
+
+  activateMenuAtCurrentSection(home);
+  activateMenuAtCurrentSection(services);
+  activateMenuAtCurrentSection(about);
+  activateMenuAtCurrentSection(contact);
+  //activateMenuAtCurrentSection(services);
  }
+
+ function activateMenuAtCurrentSection(section) {
+  //linha alvo
+  const targetLine = scrollY + innerHeight / 2
+  
+  //verificar se a seção passou da linha
+  //quais dados vou precisar?
+  
+  //o topo da seção
+  const sectionTop = section.offsetTop;
+  
+  //a altura da seção
+  const sectionHeight = section.offsetHeight;
+
+  //Topo da seção chegou ou ultrapassou a linha alto
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
+
+  //verificar se a base está abaixo da linha alvo
+  //quais dados vou precisar?
+  
+  //a seção termina onde ?
+
+  const sectionEndsAt = sectionTop + sectionHeight;
+  //o final da seção passou da linha alvo
+  const sectionEndPassedTargetLine = sectionEndsAt <=  targetLine;
+
+  //limites da seção
+  const sectionBoundaries =
+  sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine;
+
+  const sectionId = section.getAttribute("id");
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`);
+
+  menuElement.classList.remove("active");  
+  if (sectionBoundaries) {
+      menuElement.classList.add("active");
+    }
+  }
+
+
 
  function showNavOnScroll() {
   if (scrollY > 0) {
